7b4fb7ca426c71f5cf7a33c1b5ff32cb9884bebd
sompat <53874321+som-pat@users.noreply.github.com>
2022-04-04 14:36:06 +0530

    password email reset

--- rbl1/settings.py
+++ rbl1/settings.py
@@ -126,3 +126,12 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
+# SMTP
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'wms.svd@gmail.com'
+EMAIL_HOST_PASSWORD= 'svd041037'
